e5fe31cbaf4a27e95a32f3c960d0c208da05f4d9
Johh4nn <johaniza1212@gmail.com>
2024-10-04 12:03:35 -0500

    Cambio2

---
+++ src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+
+        int edad = 18;
+
+        if(edad>=18){
+            System.out.println("Ud puede realizar el proceso de votaciones");
+
+        }
+
+        System.out.println("Muchas gracias");
+    }
+}
