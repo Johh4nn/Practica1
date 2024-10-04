8d2b13292dbaf542b6d102051306969040cf6a49
Johh4nn <johaniza1212@gmail.com>
2024-10-04 12:56:06 -0500

    Datos personales y datos de la universidad

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -7,7 +7,8 @@ public class Ejercicio1 {
         System.out.println("Nombre: Johan Sebastian Vargas Iza");
         System.out.println("Direccion: Conocoto Barrio el Rosal");
         System.out.println("Numero de telefono: 0987375174");
-        System.out.println("");
+        System.out.println("Universidad: Escuela Politecnica Nacional");
+        System.out.println("Carrera: Desarrollo de Software");
 
 
     }
