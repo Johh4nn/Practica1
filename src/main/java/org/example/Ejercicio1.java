eb47915046c545a4ff828373e281ea2fa201f05a
Johh4nn <johaniza1212@gmail.com>
2024-10-04 12:41:53 -0500

    saludo

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -3,8 +3,10 @@ package org.example;
 public class Ejercicio1 {
     public static void main(String[] args) {
         // JJASJAJSJASJJAJS
+        // jasjdjasjd
         int edad=0;
 
+        System.out.println("Hola usted puede: ");
         if(edad>=18){
             System.out.println("Ud puede realizar el proceso de votaciones");
 
