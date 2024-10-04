25d5efc5110ecbbd34d180fe1eba461f4a87cfae
Johh4nn <johaniza1212@gmail.com>
2024-10-04 12:53:12 -0500

    Datos personales

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -3,12 +3,11 @@ package org.example;
 public class Ejercicio1 {
     public static void main(String[] args) {
         // JJASJAJSJASJJAJS
-        int edad=0;
-
-        if(edad>=18){
-            System.out.println("Ud puede realizar el proceso de votaciones");
-
-        }
+        //int edad=0;
+        System.out.println("Nombre: Johan Sebastian Vargas Iza");
+        System.out.println("Direccion: Conocoto Barrio el Rosal");
+        System.out.println("Numero de telefono: 0987375174");
+        System.out.println("");
 
 
     }
